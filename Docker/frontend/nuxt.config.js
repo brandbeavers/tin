8b104a44869c1adf05c7daa117a3eeda8196550a
brandbeavers <162797466+brandbeavers@users.noreply.github.com>
2024-03-17 19:03:47 +0530

    removed new line (#1) (#2)

--- Docker/frontend/nuxt.config.js
+++ Docker/frontend/nuxt.config.js
@@ -18,7 +18,6 @@ export default {
         name: 'viewport',
         content: 'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1'
       },
-      
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
