19350f3f34f8fe3c9190c8bd583c58866929e74b
Brand Beavers <brandbeavers@gmail.com>
2024-03-17 18:58:30 +0530

    testing

--- Docker/frontend/nuxt.config.js
+++ Docker/frontend/nuxt.config.js
@@ -18,6 +18,7 @@ export default {
         name: 'viewport',
         content: 'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1'
       },
+      
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
